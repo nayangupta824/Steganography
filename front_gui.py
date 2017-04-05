0a949988a126a3858870ffcfc48697ec8dab9575
nayangupta824 <nayangupta824@gmail.com>
2017-04-05 17:31:21 +0530

    A major integration with GUI

--- front_gui.py
+++ front_gui.py
@@ -4,11 +4,16 @@ import tkFileDialog
 from main import Encode , Decode
 from tkFileDialog import askopenfilename
 
-
+path_to_file = []
 
 def path():
-     askopenfilename(filetypes=[("Image File",'.png'),("Image File", '.jpg')])
+     ifile = askopenfilename(filetypes=[("Image File",'.png'),("Image File", '.jpg')])
+     path_to_file.append(ifile)
+     print path_to_file
 
+def decode(e1, key):
+	message = Decode(path_to_file, key)
+	e1.insert(0, message)
 
 def decrypt():
          root1=Tk()
@@ -18,14 +23,17 @@ def decrypt():
          Label(root1, text="Enter 16 bit Key :").grid(row=0)
          e = Entry(root1)
          e.grid(row=0, column=1)
-         pbtn = Button(root1, text='Enter Image Path', command=path)#lambda:(root1.tkFileDialog.askopenfilename(filetypes=[("Image File",'.png'),("Image File", '.jpg')]))
-         pbtn.grid(row=2, column=1, sticky=W, pady=4, padx=5)         
-         ibtn=Button(root1, text="Decode")
-         ibtn.grid(row=2, column=2, sticky=W, pady=4, padx=5)
+         Label(root1, text="Decoded message:").grid(row=2)
+         e1 = Entry(root1)
+         e1.grid(row=2, column=1)
+         pbtn = Button(root1, text='Enter Image Path', command = path)
+         pbtn.grid(row=4, column=1, sticky=W, pady=4, padx=5)         
+         ibtn=Button(root1, text="Decode",command = lambda: decode(e1, e.get()))
+         ibtn.grid(row=4, column=2, sticky=W, pady=4, padx=5)
 
 
-
-
+def test(kgp):
+	print kgp
 
 
 
@@ -36,13 +44,15 @@ def encrypt():
          root1.geometry('%dx%d+0+0'%(width,height))
          Label(root1, text="Enter 16 bit Key :").grid(row=0)
          e = Entry(root1)
+         #print e["text"]
          e.grid(row=0, column=1)
          Label(root1, text="Enter the message :").grid(row=2)
          e1 = Entry(root1)
          e1.grid(row=2, column=1)
          pbtn = Button(root1, text='Image Path', command=path)#lambda:(root1.tkFileDialog.askopenfilename(filetypes=[("Image File",'.png'),("Image File", '.jpg')]))
-         pbtn.grid(row=4, column=1, sticky=W, pady=4, padx=5)         
-         ibtn=Button(root1, text="Encode",command = Encode())
+         pbtn.grid(row=4, column=1, sticky=W, pady=4, padx=5)   
+         print path_to_file      
+         ibtn=Button(root1, text="Encode",command = lambda: Encode(path_to_file[0], "image/output2.jpg",e1.get(),e.get()))
          ibtn.grid(row=4, column=2, sticky=W, pady=4, padx=5)
 
         
